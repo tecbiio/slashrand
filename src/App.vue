0a11ebcd3ee83488b54e54566471b384cf6d4707
TecBiio <73758134+tecbiio@users.noreply.github.com>
2024-03-03 17:41:41 +0100

    UI maj

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 
-  <nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand mb-0 h1" href="#">slashrand application</a>
+  <nav id="topMenu" class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow d-flex">
+    <div class="flex-fill">
+      <h3>Slashrand Application</h3>
     </div>
   </nav>
 
   <div id="app" class="position-absolute top-50 start-50 translate-middle">
     <div class="my-3">
       <transition enter-active-class="animate__animated animate__bounceIn" leave-active-class="animate__animated animate__fadeOut animate__fast">
-        <h1 style="position: absolute; top: -40px; left: 0; right: 0;" v-if="anime">{{ previousRand }}</h1>
+        <h1 style="position: absolute; top: -40px; left: 0; right: 0;" v-if="anime">{{ nextRand }}</h1>
       </transition>
       <transition enter-active-class="animate__animated animate__bounceIn" leave-active-class="animate__animated animate__fadeOut animate__fast">
         <h1 style="position: absolute; top: -40px; left: 0; right: 0;" v-if="!anime">{{ currentRand }}</h1>
@@ -25,19 +25,18 @@
 <script>
 
 export default {
-  name: 'App',
   data() {
     return {
       anime: false,
-      previousRand: null,
+      nextRand: null,
       currentRand: null,
       max: 100
     }
   },
   methods: {
     randomize() {
-      this.previousRand = Math.floor(Math.random() * this.max) + 1;
       this.currentRand = Math.floor(Math.random() * this.max) + 1;
+      this.nextRand = Math.floor(Math.random() * this.max) + 1;
 
       this.anime = !this.anime
     }
--- src/background.js
+++ src/background.js
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -15,12 +16,15 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 200,
+    minHeight: 400,
     webPreferences: {
       
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
+      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+      // devTools: false
     }
   })
 
